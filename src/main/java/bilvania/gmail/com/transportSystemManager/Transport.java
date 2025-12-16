aacb32f47bdf7ba4cae5927a478fdcfc430cab64
Ivan <bilvania2906@gmaail.com>
2025-12-16 11:55:33 +0100

    Main transport class

---
+++ src/main/java/bilvania/gmail/com/transportSystemManager/Transport.java
@@ -0,0 +1,36 @@
+package bilvania.gmail.com.transportSystemManager;
+
+abstract public class Transport extends TransportFactory implements Moveable {
+    protected String model;
+    protected int year;
+
+    public Transport(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public abstract double getFuelConsumption();
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public double getMaxSpeed() {
+        return 0;
+    }
+}
